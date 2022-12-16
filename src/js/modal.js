86709cdbce5779ec0a376094c33ecc511e4e7fd1
Patricia Dias Baldez <patriciabaldez1985@gmail.com>
2022-12-16 17:35:30 -0300

    pasta

---
+++ src/js/modal.js
@@ -0,0 +1,7 @@
+// $('#myModal').on('shown.bs.modal', function () {
+//     $('#myInput').trigger('focus')
+//   })
+
+//   $('#myModal').on('hidden.bs.modal', function (e) {
+//     // do something...
+//   })
